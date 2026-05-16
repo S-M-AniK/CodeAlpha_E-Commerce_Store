acb03b6d4d9f910e025ecd577225388362d5ccaf
S-M-AniK <siammehedianik@Siams-MacBook-Air.local>
2026-05-17 01:15:47 +0600

    Added admin panel for products and orders

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'full_name', 'email', 'status', 'total_price', 'created']
+    list_editable = ['status']
+    inlines = [OrderItemInline]
