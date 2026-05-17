7b1d08bf8bba8495ae942b86d9e033cf0a42b259
S-M-AniK <siammehedianik@Siams-MacBook-Air.local>
2026-05-17 10:21:09 +0600

    Fixed URL routing order

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -5,9 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('products.urls', namespace='products')),
     path('cart/', include('cart.urls', namespace='cart')),
     path('orders/', include('orders.urls', namespace='orders')),
+    path('', include('products.urls', namespace='products')),
 ]
 
 if settings.DEBUG:
